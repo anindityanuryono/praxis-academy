2ad770d82670aa32ede38a5194fe554cad1d85a1
anindityanuryono <anindityanuryono@yahoo.com>
2019-12-02 16:15:00 +0700

    nambah file-ren

--- kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.ps1
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.ps1
@@ -1,7 +1,24 @@
 ﻿Write-Output "Ada file Java pada direktori" 
-Get-ChildItem -Include *.Java* -Recurse
 
+$Data = Get-ChildItem -Include *.Java* -Recurse
 
-if("ada file java pada direktori" = True){
-	Write-Output("ubah nama")
+Write-Output $Data
 
+<# Get-Date #>
+
+<# Read-Host -Prompt 'Input the user name' #>
+
+<# $Data = Read-Host -Prompt 'Input the user name' #>
+
+If ($Data  -eq $Data)  {
+	$gantiNama = Read-Host -Prompt 'Diganti namanya y / t'
+	If ($gantiNama  -eq "y")  {
+		$namaBaru = Read-Host -Prompt 'Input nama baru'
+		Write-Output $namaBaru
+		
+	}
+ 
+  } Elseif ($gantiNama  -eq "t")  {
+		Write-Output "Lanjut ke process selanjutnya"
+
+} 

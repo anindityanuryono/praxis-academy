7129189bf44a90af5751e234545436012404ac4f
anindityanuryono <anindityanuryono@yahoo.com>
2019-12-03 15:52:00 +0700

    first commit

---
+++ Minggu ke-1/Day-2/SortSourceModul.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # What is a Module?
+# 
+# Consider a module to be the same as a code library.
+# A file containing a set of functions you want to include in your application.
+# 
+# # Sort Algoritme
+# 
+# # Kasus 
+# 
+# https://medium.com/@george.seif94/a-tour-of-the-top-5-sorting-algorithms-with-python-code-43ea9aa02889
+# 
+# https://stackabuse.com/sorting-algorithms-in-python/
+
+# In[27]:
+
+
+# bubble sort - Algoritme 1
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+    
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+                    
+    return arr
+
+
+# In[30]:
+
+
+# selection sort - Algoritme 2
+
+def selection_sort(arr):        
+    for i in range(len(arr)):
+        minimum = i
+        
+        for j in range(i + 1, len(arr)):
+            # Select the smallest value
+            if arr[j] < arr[minimum]:
+                minimum = j
+
+        # Place it at the front of the 
+        # sorted end of the array
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+            
+    return arr
+
+
+# In[32]:
+
+
+# Insertion Sort - Algoritme 3
+
+def insertion_sort(arr):
+        
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
+        
+        while pos > 0 and arr[pos - 1] > cursor:
+            # Swap the number down the list
+            arr[pos] = arr[pos - 1]
+            pos = pos - 1
+        # Break and do the final swap
+        arr[pos] = cursor
+
+    return arr
+
+
+# In[34]:
+
+
+# Merge Sort - Algoritme 4
+
+def merge_sort(arr):
+    # The last array split
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    # Perform merge_sort recursively on both halves
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+
+    # Merge each side together
+    return merge(left, right, arr.copy())
+
+
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+      
+        # Sort each one and place into the result
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor+right_cursor]=left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+            
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+        
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+# In[38]:
+
+
+# Quick Sort - Algoritme 5
+
+# There are different ways to do a Quick Sort partition, this implements the
+# Hoare partition scheme. Tony Hoare also created the Quick Sort algorithm.
+def partition(nums, low, high):
+    # We select the middle element to be the pivot. Some implementations select
+    # the first element or the last element. Sometimes the median value becomes
+    # the pivot, or a random one. There are many more strategies that can be
+    # chosen or created.
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # If an element at i (on the left of the pivot) is larger than the
+        # element at j (on right right of the pivot), then swap them
+        nums[i], nums[j] = nums[j], nums[i]
+
+
+def quick_sort(nums):
+    # Create a helper function that will be called recursively
+    def _quick_sort(items, low, high):
+        if low < high:
+            # This is the index after the pivot, where our lists are split
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+
+# In[ ]:
+
+
+
+
